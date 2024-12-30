2211afa45877c4f71fbdb7e719e987a808ee1b85
scanf-pain <matus.cik@protonmail.com>
2024-12-30 15:21:47 +0100

    feat: add more image filters

--- src/filters/mainFilter.js
+++ src/filters/mainFilter.js
@@ -1,28 +1,31 @@
 import matTo3Ch from "./opencv/matTo3Ch.js";
-import matFilterMedian from "./opencv/matFilterMedian.js";
 import matFilterBrightness from "./opencv/matFilterBrightness.js";
 import matSubMat from "./opencv/matSubMat.js";
+import matFilterMedian from "./opencv/matFilterMedian.js";
+import matFilterContrast from "./opencv/matFilterContrast.js";
+import matFilterGamma from "./opencv/matFilterGamma.js";
 
 const filterCanvas = (srcCanvas, destCanvas, filters) => {
   const cv = window.cv;
-
   const srcImg = cv.imread(srcCanvas);
 
   matTo3Ch(srcImg);
 
   const filtered = srcImg.clone();
 
+  // main filter
   matFilterMedian(filtered);
 
-  if (filtered.intensity !== undefined) {
-    matFilterBrightness(filtered, filters.intensity);
-  }
+  matFilterContrast(filtered, filters.intensity);
 
   matSubMat(srcImg, filtered, filtered);
 
-  if (filtered.brightness !== undefined) {
-    matFilterBrightness(filtered, filters.brightness);
-  }
+  // additional filters
+  matFilterBrightness(filtered, filters.brightness);
+
+  matFilterContrast(filtered, filters.contrast);
+
+  matFilterGamma(filtered, filters.gamma);
 
   cv.imshow(destCanvas, filtered);
 
