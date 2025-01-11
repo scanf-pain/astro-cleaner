0664decf2afb5f5fd653cb23c6546b5fe9e5d62c
scanf-pain <matus.cik@protonmail.com>
2025-01-11 20:16:14 +0100

    fix: vite config base address

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/astro-cleaner/",
-  title: "Astro Cleaner",
+  base: "/",
 });
